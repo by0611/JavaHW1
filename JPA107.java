649854b0f5139bdea2c12a7f0c316ce3f73d8db3
by0611 <156165417+by0611@users.noreply.github.com>
2024-03-20 17:01:28 +0800

    Update JPA107.java

--- JPA107.java
+++ JPA107.java
@@ -1,12 +1,19 @@
-public class JPA107 {
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int bkb = a + b + c;
-        int bsb = 10 + b + c;
-        System.out.println("The basketball grade is "+bkb);
-        System.out.println("The baseball grade is "+bsb); 
+public class JPA107_2 {
+    public static void main(String argv[]) {//argv為參數可選的數組
+        int action = 1, skill = 2, teamgame = 3;//設置三個變數
+
+        System.out.println("The basketball grade is " + Basketball.cal(action, skill, teamgame));//輸出並呼叫函數，與其對應的數值，例如a對應的為action。
+        System.out.println("The baseball grade is " + Baseball.cal(skill, teamgame));//輸出並呼叫函數，與其對應的數值
     }
-    
 }
+
+class Basketball { //定義籃球類別
+    public static int cal(int a, int s, int t) {//定義cal函數，*cal為函數名稱，沒有任何意義。
+        return a + s + t;//回傳計算值
+    }
+}
+
+class Baseball {//定義棒球類別
+    public static int cal(int s, int t) {//定義cal函數
+        return 10 + s + t;//回傳計算值
+    }
